66d3b3c8c9fc3f32563a5b8bbce8671e4e19325d
Walmor Moreira <walmorcosta@gmail.com>
2018-08-11 19:59:48 -0300

    Add the TripController

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import AuthController from './auth/AuthController';
 import UserController from './users/UserController';
+import TripController from './trips/TripController';
 
 const router = express.Router();
 
 router.use('/auth', AuthController);
 router.use('/users', UserController);
+router.use('/trips', TripController);
 
 export default router;
---
+++ api/src/trips/TripController.js
@@ -0,0 +1,25 @@
+import express from 'express';
+
+const router = express.Router();
+
+router.get('/', async (req, res) => {
+  res.json(await req.tripService.list(req.query));
+});
+
+router.get('/travelplan', async (req, res) => {
+  res.json(await req.tripService.getTravelPlan(req.query));
+});
+
+router.get('/:id', async (req, res) => {
+  res.json(await req.tripService.get(req.params.id));
+});
+
+router.post('/', async (req, res) => {
+  res.json(await req.tripService.create(req.body));
+});
+
+router.put('/:id', async (req, res) => {
+  res.json(await req.tripService.update(req.params.id, req.body));
+});
+
+export default router;
